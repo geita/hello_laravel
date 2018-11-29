c3fede11f163227befb38372f59cedb11a626833
man  hua <zhangfby@163.com>
2018-11-29 03:07:39 +0000

    Finish user edit,update,index,and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,6 +6,13 @@ use Illuminate\Http\Request;
 use Auth;
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -21,7 +28,7 @@ class SessionsController extends Controller
        if (Auth::attempt($credentials, $request->has('remember'))) {
         //登陆成功后的相关操作
         session()->flash('success', '欢迎回来！');
-        return redirect()->route('users.show', [Auth::user()]);
+        return redirect()->intended(route('users.show', [Auth::user()]));
        } else {
         //登陆失败后的相关操作
         session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,7 +9,7 @@
 
     <div class="container">
         <div class="col-md-offset-1 col-md-10">
-             @include('users.shared._messages')
+            @include('users.shared._messages')
             @yield('content')
             @include('layouts._footer')
         </div>
