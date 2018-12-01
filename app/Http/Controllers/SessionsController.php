1713706f19eb0c5fbeb8c814a30cd5eb88111147
man  hua <zhangfby@163.com>
2018-12-01 15:16:30 +0000

    Add password resets & email configuration

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,7 +28,7 @@ class SessionsController extends Controller
        if (Auth::attempt($credentials, $request->has('remember'))) {
         //登陆成功后的相关操作
             if (Auth::user()->activated) {
-                session()->flash('success', ‘欢迎回来’);
+                session()->flash('success', '欢迎回来');
                 return redirect()->intended(route('users.show', [Auth::user()]));
             } else {
                 Auth::logout();
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -110,12 +110,10 @@ class UsersController extends Controller
     {
         $view = 'users.emails.confirm';
         $data = compact('user');
-        $from = 'aufree@yousails.com';
-        $name = 'Aufree';
         $to = $user->email;
-        $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        $subject = "感谢注册 laravel-101 应用！请确认你的邮箱。";
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
